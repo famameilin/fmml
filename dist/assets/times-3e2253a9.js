b808d6852485a2a1e5dadf7f23065567c86bc0db
famameilin <80460962+famameilin@users.noreply.github.com>
2023-08-23 18:49:36 +0800

    Delete dist directory

--- dist/assets/times-3e2253a9.js
+++
@@ -1,1 +0,0 @@
-const e=t=>`${t.getMonth()+1}月${t.getDate()}日`;export{e as f};
--- dist/assets/use-id-ad399dd1.js
+++
@@ -1,1 +0,0 @@
-import{aw as e}from"./index-335f02a3.js";let r=0;function u(){const n=e(),{name:t="unknown"}=(n==null?void 0:n.type)||{};return`${t}-${++r}`}export{u};
